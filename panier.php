e61b918ee94f1b21bd54cfb2b697a1e935a6b880
mathieubeden <mbreymand@edenschool.fr>
2020-11-10 14:39:57 +0100

    0.5.1 debut du html

--- panier.php
+++ panier.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>panier</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php 
     if(isset($_POST['user'])&&isset($_POST['pass'])){
         try{
